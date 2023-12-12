39a126229d81ee43a698d96ecd7f02591aa4184c
Hunter <Huntermatyi@gmail.com>
2023-12-12 14:59:16 -0500

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+
+    for (let i = 0; i < namesArray.length; i++) {
+        const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+
+        thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+const names = ["Hunter", "Shane", "Jesee"];
+const event = 'Birthday';
+const messages = writeCards(names, event);
+console.log(messages);
+
+function countDown() {
+    let nim = 10
+    while (nim >= 0) {
+        console.log(nim--)
+    }
+}
